63164fa2088cae94575cae21ce302546f6cd5c98
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-09-06 16:59:11 +0000

    day 2 tutorial

---
+++ 01_basics/day2.js
@@ -0,0 +1,18 @@
+let score="33abc"
+console.log(typeof score);
+let valueInNumber=Number(score);
+console.log(score)
+let sc1=null
+console.log(typeof sc1)
+console.log(valueInNumber)
+let isloggedIn=1;
+let booleanisloggedIn=Boolean(isloggedIn);
+console.log(booleanisloggedIn)
+let value=34
+let negvalue=-value
+console.log(negvalue)
+let str="priyabrata"
+let str2="khanki"
+let str3=str+str2
+console.log(str3)
+console.log("1"+2+3)
