4623f9a6532752b136cef7281ae241b4c1e7f131
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-09-09 13:01:56 +0000

    strings and all

---
+++ 01_basics/day3.js
@@ -0,0 +1,7 @@
+const name ="Sounak"
+const reponame=50
+console.log(`hello my name is {name} and my repocount is ${reponame}`)
+console.log(name.length)
+console.log(name.toUpperCase())
+const newstring=name.substring(0,4)
+console.log(newstring)
