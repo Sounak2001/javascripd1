0cb3686635631b73fce70cd8aef137625f768136
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-11-09 17:36:46 +0000

    maths

---
+++ 01_basics/day5.js
@@ -0,0 +1,12 @@
+const score = 400
+console.log(score)
+const sc=new Number(100000000.33)
+console.log(sc)
+console.log(sc.toPrecision(3))
+console.log(sc.toLocaleString('en-IN'))
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.min(2,3,4,5,6))
+const min=20
+const max=30
+console.log(Math.floor(Math.random()*(max-min+1)+min))
