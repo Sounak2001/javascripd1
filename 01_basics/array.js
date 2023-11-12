06dea2cf08508123e3c5cddeccc52125ce21b50b
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-11-12 04:32:28 +0000

    array methods

---
+++ 01_basics/array.js
@@ -0,0 +1,18 @@
+const myHero=[1,2,3,4,5,6]
+const myarray2=new Array(1,2,3,4)
+//console.log(myarray2[2])
+//myarray2.push(5)
+//myarray2.pop()
+//myHero.shift()
+//console.log(myHero.includes(9))
+//console.log(myHero.indexOf(2))
+//console.log(myHero)
+//const myH=myarray2.join()
+//console.log(myH)//myH binds the array and turns it into a string
+//console.log(myarray2)
+console.log("A",myHero);
+const n1=myHero.slice(1,3)
+//const n2=myHero.splice(1,3)
+console.log(n1);
+//console.log(n2);
+console.log("c",myHero)//splice manipulate the original array and change it
