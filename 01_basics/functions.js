ce9e9f7e1cbc3f37bc26d5fabb0e3ea662d72b2d
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-11-14 14:16:09 +0000

    new

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -1,16 +1,29 @@
-function addtwo(no1,no2)//parameters
+// function addtwo(no1,no2)//parameters
+// {
+//     return no1+no2
+// }
+// const result= addtwo(4 , 5)//function calling by giving arguments if (4 ,"5")it will gave 45 cause its became a string
+// console.log(result)
+// function bitch(username){
+//     if(username == undefined)
+//     {
+//         console.log("please enter a username");
+//         return
+//     }
+//     return username
+// }
+// console.log(bitch("Hello"))
+function calculate(...num1){//...rest operator will add every arguments
+    return num1
+}
+console.log(calculate(2,3,4))
+const user = {
+    username: "sounak",
+    price: 199
+}
+function objectshand (name)
 {
-    return no1+no2
+    console.log(`username is ${name.username} and price is ${name.price}`)//passing object in function
 }
-const result= addtwo(4 , 5)//function calling by giving arguments if (4 ,"5")it will gave 45 cause its became a string
-console.log(result)
-function bitch(username){
-    if(username == undefined)
-    {
-        console.log("please enter a username");
-        return
-    }
-    return username
-}
-console.log(bitch("Hello"))
+objectshand(user)
 
