e522e172701eeccb08c3c12e483bc3bf4b63d191
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-11-14 13:09:24 +0000

    funct

---
+++ 01_basics/functions.js
@@ -0,0 +1,16 @@
+function addtwo(no1,no2)//parameters
+{
+    return no1+no2
+}
+const result= addtwo(4 , 5)//function calling by giving arguments if (4 ,"5")it will gave 45 cause its became a string
+console.log(result)
+function bitch(username){
+    if(username == undefined)
+    {
+        console.log("please enter a username");
+        return
+    }
+    return username
+}
+console.log(bitch("Hello"))
+
