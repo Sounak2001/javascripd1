e8db4f7007efaf7f73081a567f89b0433d09db91
Sounak2001 <121948590+Sounak2001@users.noreply.github.com>
2023-11-14 09:02:51 +0000

    new

---
+++ 01_basics/objects3.0.js
@@ -0,0 +1,14 @@
+const course ={
+    coursename : "java",
+    tecaher: "sounakl",
+    price: "999"
+
+}
+const{tecaher : tech} = course//object destructurering
+console.log(course.tecaher)
+console.log(tech);
+{
+    "name":"sounak",//json key shiould be in diuble cotation as well
+    "course":"java",
+    "price": "free"
+}
